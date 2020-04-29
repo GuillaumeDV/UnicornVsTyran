ffed93fc4924b62f347979878b64a954e838d961
sandeb <sandrine.debatty@gmail.com>
2020-04-29 20:15:57 +0200

    commit to pull

--- server/index.php
+++ server/index.php
@@ -1,24 +1,17 @@
+
 <?php 
-/*header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *");
 require 'Router.php';
 include_once 'Request.php';
-include_once 'Router.php';*/
-require 'db.php';
+include_once 'Router.php';
 
-//$router = new Router(new Request);
+$router = new Router(new Request);
 
-//$router->get('/index', function() {}
-$pdo = new PDO('DSN', 'USER', 'PASS', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-if($pdo === false){
-        echo "Connection error :" . $pdo->error_log();
-} else {
-        $getAllDictateurs = "SELECT * FROM dictateurs";
-        try {
-        $sendRequest = $pdo->query($getAllDictateurs);
-        $dictateurs = $sendRequest->fetchAll(PDO::FETCH_ASSOC);
-        } catch (PDOException $e) {
-        $error = $e->getMessage();
-    }  
-}
-echo $dictateurs['name']
-?>
+$router->get('/index', function() {
+    $pdo = new PDO(DSN, USER, PASS, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+
+    $array = ['plop','plop2','plop3','plop4'];
+    return json_encode($array);
+});
+
---
+++ server/stashing.php
@@ -0,0 +1,26 @@
+<!-- <?php 
+/*header("Access-Control-Allow-Origin: *");
+require 'Router.php';
+include_once 'Request.php';
+include_once 'Router.php';*/
+// require 'db.php';
+
+//$router = new Router(new Request);
+
+//$router->get('/index', function() {}
+// $pdo = new PDO('DSN', 'USER', 'PASS', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+// if($pdo === false){
+//         echo "Connection error :" . $pdo->error_log();
+// } else {
+//         $getAllDictateurs = "SELECT * FROM dictateurs";
+//         try {
+//         $sendRequest = $pdo->query($getAllDictateurs);
+//         $dictateurs = $sendRequest->fetchAll(PDO::FETCH_ASSOC);
+//         } catch (PDOException $e) {
+//         $error = $e->getMessage();
+//     }  
+// }
+echo 'hello'
+?> -->
+
+<!-- $dictateurs['name']; -->
